0ac77da41b083ad626dc1bf7dd04b72532f3fa78
vinaydhankhar25 <55960781+vinaydhankhar25@users.noreply.github.com>
2022-03-26 23:47:11 +0530

    Add files via upload

---
+++ app/Models/Message.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    public function userFrom() {
+        return $this->belongsTo('App\User', 'user_id_from');
+    }
+
+    public function userTo() {
+        return $this->belongsTo('App\User', 'user_id_to');
+    }
+
+    public function scopeNotDeleted($query) {
+        return $query->where('deleted', false);
+    }
+
+    public function scopeDeleted($query) {
+        return $query->where('deleted', true);
+    }
+}
+
